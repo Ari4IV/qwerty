a7350dae97a845c79ed056cb36dcc1319b55ea1a
BAIN5 <50042343+BAIN5@users.noreply.github.com>
2021-01-02 13:34:53 -1000

    Add _live_stream function to qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,12 +1,20 @@
 from pynput.keyboard import Key, Controller
 
-import time
 import socket, pickle
 
 keyboard = Controller()
 
 host = '0.0.0.0'
 port = 38042
+
+
+def _live_stream(data, release_standard_value=200):
+    key = pickle.loads(data)
+    if len(data) > release_standard_value:
+        keyboard.release(key)
+    else:
+        keyboard.press(key)
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
     socket.bind((host, port))
@@ -18,8 +26,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
     with connect:
         print('Standing by', address)
         while True:
-            data = connect.recv(2048)
-            dedata = pickle.loads(data)
-            keyboard.press(dedata)
-            time.sleep(0.25)
-            keyboard.release(dedata)
+            _live_stream(connect.recv(2048))
