6727a0899e5d0a4a0c30d94a72549d010e2e1231
BAIN5 <50042343+BAIN5@users.noreply.github.com>
2021-01-05 13:25:07 -1000

    Add entry point

--- qwerty-connect.py
+++ qwerty-connect.py
@@ -4,37 +4,21 @@ import socket
 from pynput.keyboard import Key
 from pynput.keyboard import Listener
 
-host = input('IP Address? :')
-port = 38042
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((host, port))
+def main(host, port=38042):
+    with socket.create_connection((host, port)) as sock:
+        print(f'qwerty: {sock.recv(26).decode()}')
 
-qwertyMessage = str(socket.recv(2048), encoding='utf-8')
-print('qwerty:', qwertyMessage)
+        def on_press(key):
+            sock.send(pickle.dumps(key) * 1)
 
-def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
-    socket.send(pickle.dumps(key) * 1)
+        def on_release(key):
+            sock.send(pickle.dumps(key) * 2)
 
-def on_release(key):
-    print('{0} released'.format(key))
-    if key == Key.esc:
-        # Stop listener
-        return False
-    socket.send(pickle.dumps(key) * 2)
+        with Listener(on_press=on_press, on_release=on_release) as listener:
+            listener.join()
 
-# Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
 
-# ...or, in a non-blocking fashion:
-listener = Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
+if __name__ == '__main__':
+    host = input('IP Address? :')
+    main(host)
