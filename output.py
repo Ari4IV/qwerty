a29d0ad8ec56d089b35887bdeea1e5780bc997c6
BAIN5 <50042343+BAIN5@users.noreply.github.com>
2021-01-19 09:17:00 +0800

    Add output.py

---
+++ output.py
@@ -0,0 +1,13 @@
+"""The output module."""
+import colorama
+
+BANNER = fr"""{colorama.Fore.YELLOW}
+                          _         
+                         | |        
+   __ ___      _____ _ __| |_ _   _ 
+  / _` \ \ /\ / / _ \ '__| __| | | |
+ | (_| |\ V  V /  __/ |  | |_| |_| |
+  \__, | \_/\_/ \___|_|   \__|\__, |
+     | |                       __/ |
+     |_|                      |___/ 
+{colorama.Fore.RESET}"""
