71ffb02b0b39ff1f214a1a63ea6cf985776fc9fa
BAIN5 <50042343+BAIN5@users.noreply.github.com>
2021-01-20 21:10:32 +0800

    Update qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,60 +1,73 @@
-"""Copyright 2021 BAIN5"""
+"""The qwerty module."""
 import pickle
 import socket
-import sys
+import time
 
 from pynput.keyboard import Controller
 from pynput.keyboard import Listener
 
 
 class Qwerty:
-    """TODO(BAIN5)"""
-    _keyboard = Controller()
+    """The Qwerty object."""
 
     def __init__(self, host, port):
         self.host = host
         self.port = port
 
-    def _live_stream(self, data):
-        try:
-            key = pickle.loads(data)
-        except EOFError:
-            print('Good Bye')
-            sys.exit()
-        if data[-1] == 80:
-            Qwerty._keyboard.press(key)
-        else:
-            Qwerty._keyboard.release(key)
-
     def create_connection(self):
-        """TODO(BAIN5)"""
+        """The create_connection() method."""
         with socket.create_connection((self.host, self.port)) as sock:
-            print(f'qwerty: {sock.recv(26).decode()}')
+            sock.setblocking(False)
+
+            try:
+                print(f'qwerty: {sock.recv(26).decode()}')
+            except BlockingIOError:
+                pass
 
             def on_press(key):
-                sock.send(pickle.dumps(key) + b'P')
+                sock.sendall(pickle.dumps(key) + b'P')
 
             def on_release(key):
-                sock.send(pickle.dumps(key) + b'R')
+                sock.sendall(pickle.dumps(key) + b'R')
 
             with Listener(on_press=on_press, on_release=on_release) as listener:
-                try:
-                    listener.join()
-                except KeyboardInterrupt:
-                    print('Good Bye')
-                    sys.exit()
+                listener.join()
 
     def create_server(self):
-        """TODO(BAIN5)"""
+        """The create_server() method."""
         with socket.create_server((self.host, self.port)) as server:
             print('qwerty is listening at port', self.port)
-            conn, addr = server.accept()
-            print('Standing by', addr)
 
-            with conn:
-                conn.send(b'Enemy Controller Activate!')
-                while True:
-                    try:
-                        self._live_stream(conn.recv(2048))
-                    except pickle.UnpicklingError:
-                        pass
+            server.setblocking(False)
+
+            _keyboard = Controller()
+            conns = []
+
+            def _live_stream(data):
+                key = pickle.loads(data)
+
+                if data[-1] == 80:
+                    _keyboard.press(key)
+                else:
+                    _keyboard.release(key)
+
+            while True:
+                try:
+                    conn, addr = server.accept()
+                except BlockingIOError:
+                    pass
+                else:
+                    print('Standing by', addr)
+
+                    conn.setblocking(False)
+                    conn.send(b'Enemy Controller Activate!')
+
+                    conns.append(conn)
+                finally:
+                    for conn in conns:
+                        try:
+                            _live_stream(conn.recv(1024))
+                        except BlockingIOError:
+                            pass
+
+                    time.sleep(0.1)
